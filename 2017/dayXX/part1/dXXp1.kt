2aa5e4617589193c0d6b0f33461602ecf6a4aa4d
Daniel Gimenes <danielcgimenes@gmail.com>
2024-05-16 22:10:54 -0700

    d1p1 name fix + mock files

---
+++ 2017/dayXX/part1/dXXp1.kt
@@ -0,0 +1,19 @@
+package dayXX.part1
+
+import utils.*
+
+fun main() {
+    var line = readStr()
+    // var numbers = readInts()
+    while (line != null) {
+        println(doSomething(line))
+        // println(doSomethingInts(numbers))
+        line = readStr()
+        // numbers = readInts()
+    }
+}
+
+// fun doSomethingInts(numbers: String): Int {
+fun doSomething(line: String): Int {
+    return 1
+}
